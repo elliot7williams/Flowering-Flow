80cf306ec05bb5acec162d8332d64a62cc1395a3
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:55:21 -0400

    Add Flowering Flow/Flowering FlowUITests/Flowering_FlowUITestsLaunchTests.swift

---
+++ Flowering Flow/Flowering FlowUITests/Flowering_FlowUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flowering_FlowUITestsLaunchTests.swift
+//  Flowering FlowUITests
+//
+//  Created by Elliot Williams on 2025-07-12.
+//
+
+import XCTest
+
+final class Flowering_FlowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
